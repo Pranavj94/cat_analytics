eedd606bd6a59397ad666f1e8f44e4ca99894a6d
Pranavj94 <pranavj13594@gmail.com>
2025-01-30 18:06:49 +0530

    sdf

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Image from 'next/image';
 import mainIcon from './mainicon.jpg';
+import ardonagh from './ardonagh1.jpg';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center p-4 bg-white border-b border-gray-300">
+    <header className="flex justify-between items-center p-2 bg-custom-green border-b border-gray-300">
       <div className="flex items-center">
-        <Image src={mainIcon} alt="Icon" width={80} height={50} className="h-8 w-8 mr-4" />
-        <h1 className="text-xl font-bold text-black">CAT APP</h1>
+        <Image src={ardonagh} alt="Icon" width={45} height={50} className="mr-4" />
       </div>
       <Avatar className="hover:opacity-80 cursor-pointer mr-4">
         <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,75 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { FaDatabase, FaMapMarkerAlt, FaChartBar } from 'react-icons/fa';
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+} from "@/components/ui/sidebar"
+
+const items = [
+    {
+        title: 'Dummy',
+        url: '/reporting',
+        icon: FaChartBar
+      },
+  {
+    title: 'Reporting',
+    url: '/reporting',
+    icon: FaChartBar
+  },
+  {
+    title: 'Data',
+    url: '/data',
+    icon: FaDatabase
+  },
+  {
+    title: 'Data Quality',
+    url: '/dataquality',
+    icon: FaMapMarkerAlt
+  }
+];
+
+export default function AppSidebar() {
+  const pathname = usePathname();
+
+  return (
+    <Sidebar className="w-1/6 h-full bg-white border-r border-gray-200">
+      <SidebarContent className="space-y-8"> {/* Added space between sections */}
+        <SidebarGroup>
+          <SidebarGroupContent className="space-y-4"> {/* Added space between items */}
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title} className="py-2"> {/* Added vertical padding */}
+                  <SidebarMenuButton asChild>
+                    <Link 
+                      href={item.url}
+                      className={`
+                        flex items-center gap-3 px-4 py-3 
+                        transition-colors rounded-md
+                        ${pathname === item.url 
+                          ? 'bg-blue-50 text-blue-600' 
+                          : 'text-gray-600 hover:bg-gray-50 hover:text-blue-600'
+                        }
+                      `}
+                    >
+                      <item.icon className="text-lg" />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
